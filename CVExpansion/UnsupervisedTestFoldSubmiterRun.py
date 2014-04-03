4af248ede03d21f80bbabf60a50533c12212a7cd
chenyanxiong <xiongchenyan@gmail.com>
2014-04-02 22:01:06 -0400

    add condor monotir

--- CVExpansion/UnsupervisedTestFoldSubmiterRun.py
+++ CVExpansion/UnsupervisedTestFoldSubmiterRun.py
@@ -10,12 +10,20 @@ site.addsitedir('/bos/usr0/cx/PyCode/cxPyLib')
 site.addsitedir('/bos/usr0/cx/PyCode/QueryExpansion')
 
 from UnsupervisedTestFoldSubmiter import *
+from condor.CondorJobMonitor import CondorJobMonitorC
 import sys
 
 if 2 != len(sys.argv):
     print "1 para conf"
     UnsupervisedTestFoldSubmiterC.ShowConf()
     sys.exit()
+
+
+#add a check output func
+Monitor = CondorJobMonitorC()
+Monitor.User = 'cx'
+
+Monitor.Monitor() #will hold until all job finished
     
 UnsupervisedTestFoldSubmiterUnitRun(sys.argv[1])
 print "finished"
