30dd46da5bde3769121832f1f33d177230f90d9f
chenyanxiong <xiongchenyan@gmail.com>
2014-04-10 14:23:00 -0400

    pos neg feature cnt

---
+++ DataAnalysis/FeaturePosNegCnt.py
@@ -0,0 +1,73 @@
+'''
+Created on Apr 10, 2014
+calculate the prob(cnt?) of feature in positive term, in negative term, and minus
+@author: cx
+'''
+
+
+
+
+
+import site
+site.addsitedir('/bos/usr0/cx/PyCode/QueryExpansion')
+
+from base.ExpTerm import *
+import json
+import sys
+import math
+from operator import itemgetter
+
+
+if 3 != len(sys.argv):
+    print "exp term +output"
+    sys.exit()
+
+hFeatureCnt={} #feature str ->[pos,neg]
+
+
+TotalPos = 0
+TotalNeg = 0
+for line in open(sys.argv[1]):
+    ExpTerm = ExpTermC(line.strip())
+    for Feature in ExpTerm.hFeature:
+        if not Feature in hFeatureCnt:
+            hFeatureCnt[Feature] = [0,0]
+        if ExpTerm.score > 0:
+            TotalPos += 1
+            hFeatureCnt[Feature][0] += 1
+        else:
+            TotalNeg += 1
+            hFeatureCnt[Feature][1] += 1
+            
+out = open(sys.argv[2],'w')
+lFeature = []
+for feature in hFeatureCnt:
+    value = hFeatureCnt[feature]
+    value[0] = math.log(value[0] / float(TotalPos))
+    value[1] = math.log(value[1]/ float(TotalNeg))
+    lFeature.append([feature] + value + [value[0] - value[1]])
+    print >>out, feature + "\t%f\t%f\t%f" %(value[0],value[1],value[0]-value[1])
+    
+out.close()
+
+out = open(sys.argv[2] + "_TopPos",'w')
+lTop = lFeature.sort(key=itemgetter(1), reverse=True)[0:1000]
+for top in lTop:
+    print >>out, "%s\t%f\t%f\t%f" %(top[0],top[1],top[2],top[3])
+    
+out.close()
+
+out = open(sys.argv[2] + "_TopNeg",'w')
+lTop = lFeature.sort(key=itemgetter(2), reverse=True)[0:1000]
+for top in lTop:
+    print >>out, "%s\t%f\t%f\t%f" %(top[0],top[1],top[2],top[3])
+    
+out.close()
+
+
+out = open(sys.argv[2] + "_TopDiff",'w')
+lTop = lFeature.sort(key=itemgetter(3), reverse=True)[0:1000]
+for top in lTop:
+    print >>out, "%s\t%f\t%f\t%f" %(top[0],top[1],top[2],top[3])
+    
+out.close()    
