1ecd2049e3c0aa962a68b4d89abf9c3efc4488f3
chenyanxiong <xiongchenyan@gmail.com>
2014-05-11 21:26:33 -0400

    debug

--- ExpansionAlg/ExpansionSingleRunCenter.py
+++ ExpansionAlg/ExpansionSingleRunCenter.py
@@ -57,7 +57,7 @@ class ExpansionSingleRunCenterC(cxBaseC):
         self.NumOfReRankDoc = int(conf.GetConf('rerankdepth',self.NumOfReRankDoc))        
         self.ExpansionMethod = conf.GetConf('expmethod',self.ExpansionMethod)
         self.InputType = conf.GetConf('inputtype')
-        self.OutExpTerm  = bool(int(conf.GetConf('outexpterm'),0))
+        self.OutExpTerm  = bool(int(conf.GetConf('outexpterm',0)))
         self.NumOfExpTerm = int(conf.GetConf('numofexpterm'),self.NumOfExpTerm)
         if not os.path.exists(self.EvaOutDir):
             os.makedirs(self.EvaOutDir)
